1b583b802623f3fb30422d022df5f6bacd1cd7e6
kalpit10 <kalpit.swami@gmail.com>
2024-04-29 20:48:19 +0530

    Added a new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Home from "./components/Home";
 // import Contact from "./components/Contact";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Story from "./components/Story";
+import Team from "./components/Team";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/ourstory" element={<Story />} />
+          <Route path="/faculty" element={<Team />} />
         </Routes>
       </BrowserRouter>
     </div>
