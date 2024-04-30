452c2f6a8ceb75cf4118936004abf29bbf33b682
kalpit10 <kalpit.swami@gmail.com>
2024-04-30 19:48:11 +0530

    Made styling changes to modal

--- frontend/src/side-components-faculty/NavitaTewariModal.jsx
+++ frontend/src/side-components-faculty/NavitaTewariModal.jsx
@@ -7,7 +7,7 @@ function NavitaTewariModal({ isVisible, onClose }) {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 bg-zinc-950 bg-opacity-35 backdrop-blur-lg flex justify-center items-center h-full">
+    <div className="fixed inset-0 bg-zinc-950 bg-opacity-35 backdrop-blur-lg flex justify-center items-center h-full z-10">
       <div className="text-white w-11/12 h-5/6 overflow-y-auto rounded-lg p-4">
         <div className="-ml-3 p-4 flex items-center">
           <img
