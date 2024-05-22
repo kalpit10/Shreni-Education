64117d78e0349c0736e16d39fa05ad1a04ad82a6
kalpit10 <kalpit.swami@gmail.com>
2024-05-22 18:32:37 +0530

    Added heading and HR

--- frontend/src/components/Story.jsx
+++ frontend/src/components/Story.jsx
@@ -3,11 +3,18 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import OurStory from "../side-components-story/OurStory";
 import StudentCount from "../side-components-story/StudentCount";
+import HorizontalLine from "../side-components-home/HorizontalLine";
 
 function Story() {
   return (
-    <div>
+    <div className="bg-[#FFFAFA]">
       <Header />
+      <h1 className="text-center p-4 text-3xl font-bold text-[#2e3192]">
+        OUR JOURNEY
+      </h1>
+      <div className="relative negative-margin">
+        <HorizontalLine />
+      </div>
       <OurStory />
       <StudentCount />
       <Footer />
