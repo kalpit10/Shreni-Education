c6aa54e55490ec4d15a5d7c02929a6298c235314
kalpit10 <kalpit.swami@gmail.com>
2024-05-25 21:05:30 +0530

    Added a new button

--- frontend/src/side-components-home/CurriculumSchool.jsx
+++ frontend/src/side-components-home/CurriculumSchool.jsx
@@ -9,6 +9,12 @@ function CurriculumSchool() {
   const handleClick = () => {
     navigate("/school-curriculum");
   };
+
+  const linkClick = () => {
+    window.location.href =
+      "https://www.cambridgeinternational.org/programmes-and-qualifications/cambridge-upper-secondary/cambridge-igcse/curriculum/";
+  };
+
   return (
     <div className="bg-white shadow-lg rounded-lg max-w-4xl mx-auto overflow-hidden mt-5 transform transition-transform duration-300 hover:scale-105 2xl:max-w-5xl">
       <div className="container mx-auto flex flex-col md:flex-row py-8 px-4">
@@ -17,29 +23,28 @@ function CurriculumSchool() {
         </div>
 
         {/* Course Details Section */}
-        <div className="md:w-1/2 md:pl-8 md:order-1">
+        <div className="md:w-1/2 md:pl-8 md:order-1 justify-center">
           <h1 className="text-3xl font-bold text-red-500 mb-4 mt-5 hover-underline-animation-home">
             School Curriculum
           </h1>
           <p className="text-lg text-gray-700 leading-relaxed">
-            Students from Classes III to XII across CBSE, ICSE, and ISC boards
-            identify their needs, learning styles and the pace at which learning
-            comes comfortably to them. Our classes focus on every individual as
-            a unique personality who needs to be nurtured, shown around the
-            world, left alone to experience and carve a niche for themselves
-            years after they stop going to school.
+            Students from Classes I to XII across CBSE, ICSE, ISC and IGCSE
+            boards identify their needs, learning styles and the pace at which
+            learning comes comfortably to them. Our classes focus on every
+            individual as a unique personality who needs to be nurtured, shown
+            around the world, left alone to experience and carve a niche for
+            themselves years after they stop going to school.
           </p>
+          <div className="flex md:justify-start text-center pb-8 mt-3">
+            <button className="classy-button-cur mr-3" onClick={handleClick}>
+              Our Curriculum
+            </button>
+            <button className="classy-button-cur" onClick={linkClick}>
+              Official Website
+            </button>
+          </div>
         </div>
-      </div>
-
-      {/* Learn More Button Section */}
-      <div className="text-center pb-8 md:ml-44">
-        <button
-          onClick={handleClick}
-          className="classy-button-cur"
-        >
-          Learn More
-        </button>
+        {/* Learn More Button Section */}
       </div>
     </div>
   );
