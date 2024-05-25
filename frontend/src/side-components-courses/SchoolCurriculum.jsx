453cbe493e212c2fc07c6521e11116b0ffd8933b
kalpit10 <kalpit.swami@gmail.com>
2024-05-26 00:45:58 +0530

    Added Few More Subjects

--- frontend/src/side-components-courses/SchoolCurriculum.jsx
+++ frontend/src/side-components-courses/SchoolCurriculum.jsx
@@ -7,7 +7,13 @@ import SocialStudies from "./side-components-schoolcurriculum/SocialStudies";
 import Psychology from "./side-components-schoolcurriculum/Psychology";
 import Accountancy from "./side-components-schoolcurriculum/Accountancy";
 import IncomeTax from "./side-components-schoolcurriculum/IncomeTax";
+import Economics from "./side-components-schoolcurriculum/Economics";
 import { Typography } from "@mui/material";
+import BST from "./side-components-schoolcurriculum/BST";
+import Geography from "./side-components-schoolcurriculum/Geography";
+import History from "./side-components-schoolcurriculum/History";
+import PolScience from "./side-components-schoolcurriculum/PolScience";
+import HindiSanskrit from "./side-components-schoolcurriculum/HindiSanskrit";
 
 const SchoolCurriculum = () => {
   return (
@@ -52,7 +58,26 @@ const SchoolCurriculum = () => {
           <div className="-mt-24">
             <IncomeTax />
           </div>
+          <div className="-mt-24">
+            <Economics />
+          </div>
+          <div className="-mt-24">
+            <BST />
+          </div>
+          <div className="-mt-0">
+            <Geography />
+          </div>
+          <div className="">
+            <History />
+          </div>
+          <div className="">
+            <PolScience />
+          </div>
+          <div className="">
+            <HindiSanskrit />
+          </div>
         </div>
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8"></div>
       </div>
       <Footer />
     </div>
