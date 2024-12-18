808ad92060f18f1321d1c8fba755a06570aa3fe4
kalpit10 <kalpit.swami@gmail.com>
2024-12-18 17:59:34 -0500

    Reduced the file structure using props into one single file

--- frontend/src/side-components-courses/side-components-SE/ExamList.jsx
+++ frontend/src/side-components-courses/side-components-SE/ExamList.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import SE1 from "../../images/SE1.png";
 
-function IELTS() {
+function ExamList({
+  img,
+  title,
+  subtitle,
+  description,
+  subject,
+  linkExamWebsite,
+}) {
   const linkClick = () => {
-    window.location.href = "https://ielts.org/";
+    // Open the link in a new tab
+    window.open(linkExamWebsite, "_blank");
   };
-
   return (
     <div className="mt-12">
       <div className="container">
-        <img src={SE1} alt="IELTS" className="h-52" />
+        <img src={img} alt="IELTS" className="h-52" loading="lazy" />
         <h1 className="text-2xl font-bold font-serif mt-4 mx-auto w-11/12">
-          IELTS (International English Language Testing System)
+          {title}
         </h1>
         <p className="mt-4 w-11/12 mx-auto">
-          The <span className="font-bold">IELTS</span> exam is for those who
-          either wish to study further or work abroad. It is a requirement for
-          proving your proficiency in english all around the world.
+          The <span className="font-bold">{subtitle}</span> {description} <br />
+          <br />
+          <span className="font-semibold">{subject}</span>
         </p>
         <div className="flex text-center pb-8 mt-4">
           <button className="classy-button-cur mx-auto" onClick={linkClick}>
@@ -28,4 +34,4 @@ function IELTS() {
   );
 }
 
-export default IELTS;
+export default ExamList;
